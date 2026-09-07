ae8301330a775e831692120a08e1280d5c9a329c
Albert <iangirovalbert98@gmail.com>
2026-09-08 00:56:17 +0200

    Bump app version to 0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.euclab.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
     }
 
     buildFeatures {
